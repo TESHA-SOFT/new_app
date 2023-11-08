27b96e7cbbfb5682d576713433c91d934f232d08
Akilbayeva Anna <akilbayeva.anna@mail.ru>
2023-11-09 01:09:24 +0300

    Прописаны пути к новым страницам

---
+++ lib/auth/routs/my_app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../pages/view_pages.dart';
+import '../style/view_style.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColor.background,
+        fontFamily: 'Nunito',
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/':(context) => const StartPage(),
+        '/login':(context) => const LoginPage(),
+        '/signup':(context) => const SignupPage(),
+        '/verif_ph':(context) => const PhoneVerification(),
+        '/forgot_pas':(context) => const ForgorPassword(),
+        '/code':(context) => const CodePage(),
+      },
+    );
+  }
+}
