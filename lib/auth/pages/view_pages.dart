6e7b83ece094ffa65486131d1225daced90b3e53
Akilbayeva Anna <akilbayeva.anna@mail.ru>
2023-11-11 23:12:33 +0300

    Прописаны пути и передача для новых страниц

--- lib/auth/pages/view_pages.dart
+++ lib/auth/pages/view_pages.dart
@@ -1,6 +1,7 @@
 export 'log in/forgot_password_page.dart';
-export 'sign up/code_page.dart';
+export 'code_page.dart';
 export 'start_page.dart';
 export 'log in/login_page.dart';
 export 'sign up/phone_verification_page.dart';
 export 'sign up/signup_page.dart';
+export 'log in/reset_password_page.dart';
--- lib/auth/routs/my_app.dart
+++ lib/auth/routs/my_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../main_pages/view_main_page.dart';
 import '../pages/view_pages.dart';
 import '../style/view_style.dart';
 
@@ -22,6 +23,8 @@ class MyApp extends StatelessWidget {
         '/verif_ph':(context) => const PhoneVerification(),
         '/forgot_pas':(context) => const ForgorPassword(),
         '/code':(context) => const CodePage(),
+        '/reset_pas':(context) => const ResetPassword(),
+        '/main_page':(context) => const MainPage(),
       },
     );
   }
