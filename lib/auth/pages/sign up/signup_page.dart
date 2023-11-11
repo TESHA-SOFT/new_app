5c882a9b72ad4753c1a62093f3651472739c801d
Akilbayeva Anna <akilbayeva.anna@mail.ru>
2023-11-11 23:15:43 +0300

    Улучшена проверка с помощью _formkey

--- lib/auth/pages/sign up/signup_page.dart
+++ lib/auth/pages/sign up/signup_page.dart
@@ -18,9 +18,9 @@ class _SignupPageState extends State<SignupPage> {
   bool isobscureText1 = true;
   String? _password = '';
   String? _confirmPassword = '';
-  String? _username = '';
   RegExp emailRegax = RegExp(r'^[\w\.-]+@[\w-]+\.\w{2,3}(\.\w{2,3})?$');
   late bool isEmailValid;
+  final _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -33,181 +33,181 @@ class _SignupPageState extends State<SignupPage> {
             child: IntrinsicHeight(
               child: Padding(
                 padding: EdgeInsets.all(25),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Spacer(),
-                      SvgPicture.asset('assets/temp/logo.svg', width: 90,),
-                      const Spacer(),
-                      Text(
-                        'Sign up',
-                        style: TextStyle(
-                          fontSize: 38,
-                          fontWeight: FontWeight.w600,
-                          color: AppColor.font2,
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Spacer(),
+                        SvgPicture.asset('assets/temp/logo.svg', width: 90,),
+                        const Spacer(),
+                        Text(
+                          'Sign up',
+                          style: TextStyle(
+                            fontSize: 38,
+                            fontWeight: FontWeight.w600,
+                            color: AppColor.font2,
+                          ),
                         ),
-                      ),
-                      const Spacer(),
-                      Text(
-                        textAlign: TextAlign.center,
-                        'By signing up, you agree to our',
-                        style: TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w600,
-                          color: AppColor.font2,
+                        const Spacer(),
+                        Text(
+                          textAlign: TextAlign.center,
+                          'By signing up, you agree to our',
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600,
+                            color: AppColor.font2,
+                          ),
                         ),
-                      ),
-                      TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Term and privacy policy',
-                            style: TextStyle(
-                              height: 0.5,
-                              fontSize: 17,
-                              fontWeight: FontWeight.w600,
-                              color: AppColor.button2,
+                        TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Term and privacy policy',
+                              style: TextStyle(
+                                height: 0.5,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w600,
+                                color: AppColor.button2,
+                              ),
+                            )),
+                        const Spacer(),
+                        TextFormField(
+                          style: TextStyle(color: AppColor.font2),
+                          decoration: DecorationLogApp().decor('User name'),
+                          validator: (value) => ValidApp().validateUsername(value),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: AppColor.font2),
+                          decoration: DecorationLogApp().decor('Email address'),
+                          onChanged: (value){
+                            isEmailValid = emailRegax.hasMatch(value);
+                          },
+                          validator: (value) => ValidApp().validateEmail(value),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          obscureText: isobscureText,
+                          style: TextStyle(color: AppColor.font2),
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            hintStyle: TextStyle(color: AppColor.font2),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: AppColor.button1)),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: AppColor.font2),
                             ),
-                          )),
-                      const Spacer(),
-                      TextFormField(
-                        style: TextStyle(color: AppColor.font2),
-                        decoration: DecorationLogApp().decor('User name'),
-                        onChanged: (value) {
-                          _username = value;
-                        },
-                        validator: (value) => ValidApp().validateUsername(value),
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                        style: TextStyle(color: AppColor.font2),
-                        decoration: DecorationLogApp().decor('Email address'),
-                        onChanged: (value){
-                          isEmailValid = emailRegax.hasMatch(value);
-                        },
-                        validator: (value) => ValidApp().validateEmail(value),
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                        obscureText: isobscureText,
-                        style: TextStyle(color: AppColor.font2),
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          hintStyle: TextStyle(color: AppColor.font2),
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: AppColor.button1)),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: AppColor.font2),
+                            suffixIcon: IconButton(
+                              splashColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                              onPressed: () {
+                                setState(() {
+                                  isobscureText = !isobscureText;
+                                }
+                                );
+                              },
+                              icon: isobscureText
+                                  ? SvgPicture.asset('assets/icon/eye-closed.svg')
+                                  : SvgPicture.asset('assets/icon/eye.svg'),
+                            )
                           ),
-                          suffixIcon: IconButton(
-                            splashColor: Colors.transparent,
-                            highlightColor: Colors.transparent,
-                            onPressed: () {
-                              setState(() {
-                                isobscureText = !isobscureText;
-                              }
-                              );
-                            },
-                            icon: isobscureText
-                                ? SvgPicture.asset('assets/icon/eye-closed.svg')
-                                : SvgPicture.asset('assets/icon/eye.svg'),
-                          )
+                          onChanged: (value) {
+                            _password = value;
+                          },
+                          validator: (value) => ValidApp().validatePassword(value),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
                         ),
-                        onChanged: (value) {
-                          _password = value;
-                        },
-                        validator: (value) => ValidApp().validatePassword(value),
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                        obscureText: isobscureText1,
-                        style: TextStyle(color: AppColor.font2),
-                        decoration: InputDecoration(
-                          hintText: 'Confirm password',
-                          hintStyle: TextStyle(color: AppColor.font2),
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: AppColor.button1)),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: AppColor.font2),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          obscureText: isobscureText1,
+                          style: TextStyle(color: AppColor.font2),
+                          decoration: InputDecoration(
+                            hintText: 'Confirm password',
+                            hintStyle: TextStyle(color: AppColor.font2),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: AppColor.button1)),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: AppColor.font2),
+                            ),
+                            suffixIcon: IconButton(
+                              splashColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                              onPressed: () {
+                                setState(() {
+                                  isobscureText1 = !isobscureText1;
+                                }
+                                );
+                              },
+                              icon: isobscureText1
+                                  ? SvgPicture.asset('assets/icon/eye-closed.svg')
+                                  : SvgPicture.asset('assets/icon/eye.svg'),
+                            )
                           ),
-                          suffixIcon: IconButton(
-                            splashColor: Colors.transparent,
-                            highlightColor: Colors.transparent,
-                            onPressed: () {
-                              setState(() {
-                                isobscureText1 = !isobscureText1;
-                              }
-                              );
-                            },
-                            icon: isobscureText1
-                                ? SvgPicture.asset('assets/icon/eye-closed.svg')
-                                : SvgPicture.asset('assets/icon/eye.svg'),
-                          )
-                        ),
-                        onChanged: (value) {
-                          _confirmPassword = value;
-                        },
-                        validator: (value) {
-                          if (value!.length < 8) {
-                            return 'The password must be at least 8 characters long';
-                          }
-                          if (_confirmPassword != _password) {
-                            return 'Confirm password not matching';
-                          }
-                          return null;
-                        },
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                      ),
-                      const Spacer(),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: (){
-                            if(_password!.length >= 8 && _confirmPassword == _password && isEmailValid && _username != ''){
-                              Navigator.of(context).pushNamed('/verif_ph');
+                          onChanged: (value) {
+                            _confirmPassword = value;
+                          },
+                          validator: (value) {
+                            if (value!.length < 8) {
+                              return 'The password must be at least 8 characters long';
                             }
-                          }, 
-                          child: Text(
-                            'Sign up',
-                            style: AppFont.header2,
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: AppColor.font2,
-                            backgroundColor: AppColor.button2,
+                            if (_confirmPassword != _password) {
+                              return 'Confirm password not matching';
+                            }
+                            return null;
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                        ),
+                        const Spacer(),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: (){
+                              if(_formkey.currentState!.validate()){
+                                Navigator.of(context).pushNamed('/verif_ph');
+                              }
+                            }, 
+                            child: Text(
+                              'Sign up',
+                              style: AppFont.header2,
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              foregroundColor: AppColor.font2,
+                              backgroundColor: AppColor.button2,
+                            ),
                           ),
                         ),
-                      ),
-                      const Spacer(),
-                      Text(
-                        'or connect with',
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: AppColor.font2,
+                        const Spacer(),
+                        Text(
+                          'or connect with',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColor.font2,
+                          ),
                         ),
-                      ),
-                      const Spacer(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          DecorationLogApp().emailButton('assets/temp/google.png'),
-                          DecorationLogApp().emailButton('assets/temp/vk.png'),
-                          DecorationLogApp().emailButton('assets/temp/yandex.png'),
-                        ],
-                      ),
-                      const Spacer(),
-                    ],
+                        const Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            DecorationLogApp().emailButton('assets/temp/google.png'),
+                            DecorationLogApp().emailButton('assets/temp/vk.png'),
+                            DecorationLogApp().emailButton('assets/temp/yandex.png'),
+                          ],
+                        ),
+                        const Spacer(),
+                      ],
+                  ),
                 ),
               ),
             ),
