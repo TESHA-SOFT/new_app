e79e3946fc25d0b980cff1fa8dfaf00e2e15432f
Akilbayeva Anna <akilbayeva.anna@mail.ru>
2023-11-11 23:13:07 +0300

    Упрощен класс ForgotPassword

--- lib/auth/pages/log in/forgot_password_page.dart
+++ lib/auth/pages/log in/forgot_password_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../components/view_components.dart';
 
-
 class ForgorPassword extends StatefulWidget {
   const ForgorPassword({super.key});
 
@@ -16,20 +15,7 @@ class _ForgorPasswordState extends State<ForgorPassword> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Verification.numberVerif(),
-      body: LayoutBuilder(
-        builder: (context, constraints) =>
-        SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(minHeight: constraints.maxHeight),
-            child: IntrinsicHeight(
-              child: Padding(
-                padding: EdgeInsets.all(15),
-                child: Verification.numberVerifi('Forgot your password?','/code', context, phone),
-              )
-            ),
-          ),
-        )
-      ),
+      body: Verification.layoutVerif(phone, 'Forgot your password?\nEnter your mobile number', '/reset_pas'),
     );
   }
 }
