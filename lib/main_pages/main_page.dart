cb1e4989106c389dc6644d48ed3be7139c4f82c7
Akilbayeva Anna <akilbayeva.anna@mail.ru>
2023-11-22 00:05:33 +0300

    feat: Add basic MainPage layout

--- lib/main_pages/main_page.dart
+++ lib/main_pages/main_page.dart
@@ -1,10 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final user = FirebaseAuth.instance.currentUser!;
+
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: Center(
+        child: Text(
+        'LOGED IN' + user.email!,
+        style: TextStyle(
+            fontSize: 28,
+            color: Colors.white
+          ),
+      )),
+    );
   }
 }
